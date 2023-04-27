54f5d591b25ad2e778731dcdf18c2af00ecde02d
Joaquin Olazabal Abugattas <joaquin_oa@hotmail.com>
2023-04-27 06:51:59 -0400

    Skybox, enviorment

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -19,11 +19,6 @@ public class Door : MonoBehaviour
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Player" && gameManager.instance.enemiesRemaining <= 0) {
             SceneManager.LoadScene("Joaquin Test");
-        }
-    }
-
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Player" && gameManager.instance.enemiesRemaining <= 0) {
             gameManager.instance.UpdateGameGoal(0);
         }
     }
