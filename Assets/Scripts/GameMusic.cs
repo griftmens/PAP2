d24f802857fcfa650abf04099372c6a4998912e0
Joaquin Olazabal Abugattas <joaquin_oa@hotmail.com>
2023-05-19 17:43:30 -0400

    SFX FINAL fix

--- Assets/Scripts/GameMusic.cs
+++ Assets/Scripts/GameMusic.cs
@@ -8,8 +8,14 @@ public class GameMusic : MonoBehaviour
 
     public AudioSource musicGame;
     public AudioMixer musicMixer;
+    public AudioSource sfxGame;
+    public AudioMixer sfxMixer;
 
     public void ControlMusicGame(float sliderMusicG){
         musicMixer.SetFloat("GameMusicVol", Mathf.Log10(sliderMusicG) * 10);
+    }
+
+    public void ControlSFXGame(float sliderSFXGame){
+        sfxMixer.SetFloat("SFXVol", Mathf.Log10(sliderSFXGame) * 10);
     }
 }
