2be33e4e5c9bf9b80cb06d9af2acc801fc1c64ed
Joaquin Olazabal Abugattas <joaquin_oa@hotmail.com>
2023-05-20 18:01:55 -0400

    Audio Source laser strike

--- Assets/Scripts/LaserStrike.cs
+++ Assets/Scripts/LaserStrike.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class LaserStrike : MonoBehaviour
 {
     bool isReady;
+    public AudioSource laserSound;
     void Start()
     {
         isReady = true;
         Destroy(gameObject, gameManager.instance.playerScript.laserstrikeTime);
+        laserSound = GetComponent<AudioSource>();
     }
 
     public void OnTriggerStay(Collider other)
     {
+        laserSound.Play();
         IDamage Damageable = other.GetComponent<IDamage>();
         if (Damageable != null && isReady && other.GetType() == typeof(CapsuleCollider))
         {
