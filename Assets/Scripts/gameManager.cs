46b0ab1aab9493af6752728670a0374bb991b109
IndieCix <tonyposadas1@gmail.com>
2023-05-26 19:58:34 -0400

    Pause fix escape twice

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -139,6 +139,7 @@ public class gameManager : MonoBehaviour
         Time.timeScale = timeScaleOg;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        isPaused = false;
         activeMenu.SetActive(false);
         activeMenu = null;
     }
